2699f791b8f1edd721ed6a99317305dcf77eca81
pbacg459 <154271670+pbacg459@users.noreply.github.com>
2023-12-18 11:32:12 -0800

    Update streamlit_app.py
    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My parents healthy dinner menu')
 
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Peanuts, Sunflower seeds and Pumpkin seeds')
