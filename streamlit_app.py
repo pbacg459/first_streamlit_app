6f85d692b00397d3b2697a71deb05f487d397018
pbacg459 <154271670+pbacg459@users.noreply.github.com>
2023-12-18 11:38:02 -0800

    Update streamlit_app.py
    updated with logos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My parents healthy dinner menu')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
-streamlit.text('Peanuts, Sunflower seeds and Pumpkin seeds')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado toast')
