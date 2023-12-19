e03d89ece4c0cde9caa5c4a4cac3e89797534355
pbacg459 <154271670+pbacg459@users.noreply.github.com>
2023-12-19 10:51:06 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,16 @@ streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Let's put a pick list here so they can pick the fruit they want to include 
-#my_fruit_list = my_fruit_list.set_index('Fruit')
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Plums','Pineapple'])
+my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Plums','Pineapple'])
 
 # Display the table on the page.
-#streamlit.dataframe(my_fruit_list)
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
